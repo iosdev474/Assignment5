4061fa7fc51bf2ad7589dc75c08a3dbe900c7e93
iOSDev <33452790+iosdev474@users.noreply.github.com>
2017-11-08 11:30:41 +0530

    Update q13.cpp

--- q13.cpp
+++ q13.cpp
@@ -1,21 +1,5 @@
 #include <stdio.h>
- 
-int fun(int arr[], int n)
-{
-	int sum=arr[0];
-	int max=arr[0];
-	int i;
-	for(i=0;i<n;i++)
-	{
-		sum+=arr[i];
-		if(sum<0)
-			sum=0;
-		if(max<sum)
-			max=sum;
-	}
-	return max;
-}
- 
+
 int main()
 {
 	int n,x,i,j;
